2199b6787490e36801f3c9a02f4a106481dd2cff
github-action-benchmark <github@users.noreply.github.com>
2024-10-01 02:58:58 +0000

    add Lance Rust Benchmarks (cargo) benchmark result for d08e474e7e5038cfe6cce44ae5639471c553e39d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727751252258,
+  "lastUpdate": 1727751538896,
   "repoUrl": "https://github.com/lancedb/lance",
   "entries": {
     "Lance Rust Benchmarks": [
@@ -79,6 +79,86 @@ window.BENCHMARK_DATA = {
             "name": "L2(uint8, auto-vectorization)",
             "value": 143626479,
             "range": "± 850723",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "lancedb",
+            "username": "lancedb"
+          },
+          "committer": {
+            "name": "lancedb",
+            "username": "lancedb"
+          },
+          "id": "d08e474e7e5038cfe6cce44ae5639471c553e39d",
+          "message": "ci: run rust benchmark and generate report",
+          "timestamp": "2024-10-01T01:20:49Z",
+          "url": "https://github.com/lancedb/lance/pull/2964/commits/d08e474e7e5038cfe6cce44ae5639471c553e39d"
+        },
+        "date": 1727751538024,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "L2(half::binary16::f16, scalar)",
+            "value": 4447666835,
+            "range": "± 14309029",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "L2(half::binary16::f16, auto-vectorization)",
+            "value": 293705952,
+            "range": "± 460801",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "L2(f32, scalar)",
+            "value": 664593825,
+            "range": "± 1545041",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "L2(f32, auto-vectorization)",
+            "value": 356393988,
+            "range": "± 416485",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "L2(f32, simd)",
+            "value": 378784983,
+            "range": "± 2668931",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "L2(f64, scalar)",
+            "value": 1051051141,
+            "range": "± 3198500",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "L2(f64, auto-vectorization)",
+            "value": 655341430,
+            "range": "± 422134",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "L2(simd,f32x8)",
+            "value": 3620941,
+            "range": "± 7823",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "L2(uint8, scalar)",
+            "value": 139266300,
+            "range": "± 358385",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "L2(uint8, auto-vectorization)",
+            "value": 142838411,
+            "range": "± 1144632",
             "unit": "ns/iter"
           }
         ]
