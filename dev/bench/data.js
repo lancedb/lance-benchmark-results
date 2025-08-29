c970c90f9f37fe2df066d99d3b890f0e4fb2ae0b
github-action-benchmark <github@users.noreply.github.com>
2025-08-29 09:29:14 +0000

    add Lance Rust Benchmarks (cargo) benchmark result for 0b82c816d1b3cf5a60a42b7cfa525c2df971aa7f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756373361253,
+  "lastUpdate": 1756459754812,
   "repoUrl": "https://github.com/lancedb/lance",
   "entries": {
     "Lance Rust Benchmarks": [
@@ -85047,6 +85047,118 @@ window.BENCHMARK_DATA = {
             "name": "ScalarQuantizationStorage,chunks=1024x10K",
             "value": 309,
             "range": "± 1",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "nathan.ma",
+            "username": "majin1102",
+            "email": "majin1102@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "0b82c816d1b3cf5a60a42b7cfa525c2df971aa7f",
+          "message": "fix: validate operations in transaction commit (#4532)",
+          "timestamp": "2025-08-29T05:56:00Z",
+          "url": "https://github.com/lancedb/lance/commit/0b82c816d1b3cf5a60a42b7cfa525c2df971aa7f"
+        },
+        "date": 1756459753925,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "create_hnsw(10240x512,levels=6)",
+            "value": 7709564716,
+            "range": "± 68902943",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "search_hnsw10240x512, levels=6",
+            "value": 822895,
+            "range": "± 28066",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "invert_indexing(1000000)",
+            "value": 42808333985,
+            "range": "± 833917705",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "invert_search(1000000)",
+            "value": 37672,
+            "range": "± 282",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "l2,32768",
+            "value": 4579574477,
+            "range": "± 2136831",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "dot,32768",
+            "value": 2235340741,
+            "range": "± 530178",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "construct_dist_table: l2,PQ=96,DIM=1536",
+            "value": 130681,
+            "range": "± 49",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "construct_dist_table: dot,PQ=96,DIM=1536",
+            "value": 195992,
+            "range": "± 990",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compute_distances: 16000,l2,PQ=96,DIM=1536",
+            "value": 833204,
+            "range": "± 429",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compute_distances: 16000,cosine,PQ=96,DIM=1536",
+            "value": 851905,
+            "range": "± 481",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compute_distances: 16000,dot,PQ=96,DIM=1536",
+            "value": 866323,
+            "range": "± 323",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ScalarQuantizationStorage,chunks=1x10K",
+            "value": 289,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ScalarQuantizationStorage,chunks=32x10K",
+            "value": 301,
+            "range": "± 6",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ScalarQuantizationStorage,chunks=128x10K",
+            "value": 294,
+            "range": "± 20",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ScalarQuantizationStorage,chunks=1024x10K",
+            "value": 295,
+            "range": "± 16",
             "unit": "ns/iter"
           }
         ]
