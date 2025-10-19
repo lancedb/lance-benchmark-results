ca350645d3aea471b08b1f83c2687e0b120a8142
github-action-benchmark <github@users.noreply.github.com>
2025-10-19 09:29:33 +0000

    add Lance Rust Benchmarks (cargo) benchmark result for 4d6b58c22d3f9eb788955bf2e6419f529e0cb9b1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760779801279,
+  "lastUpdate": 1760866173462,
   "repoUrl": "https://github.com/lancedb/lance",
   "entries": {
     "Lance Rust Benchmarks": [
@@ -90755,6 +90755,118 @@ window.BENCHMARK_DATA = {
             "name": "ScalarQuantizationStorage,chunks=1024x10K",
             "value": 305,
             "range": "± 2",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "nathan.ma",
+            "username": "majin1102",
+            "email": "majin1102@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "4d6b58c22d3f9eb788955bf2e6419f529e0cb9b1",
+          "message": "feat(java): support shallow_clone (#4962)\n\nClose #4854\n\n---------\n\nCo-authored-by: majin.nathan <majin.nathan@bytedance.com>",
+          "timestamp": "2025-10-17T22:23:52Z",
+          "url": "https://github.com/lancedb/lance/commit/4d6b58c22d3f9eb788955bf2e6419f529e0cb9b1"
+        },
+        "date": 1760866172645,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "create_hnsw(10240x512,levels=6)",
+            "value": 6804626431,
+            "range": "± 66106196",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "search_hnsw10240x512, levels=6",
+            "value": 847290,
+            "range": "± 20422",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "invert_indexing(1000000)",
+            "value": 45334427263,
+            "range": "± 599035420",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "invert_search(1000000)",
+            "value": 73760,
+            "range": "± 1063",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "l2,32768",
+            "value": 4583960180,
+            "range": "± 11682737",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "dot,32768",
+            "value": 2243636683,
+            "range": "± 6814098",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "construct_dist_table: l2,PQ=16,DIM=128",
+            "value": 17636,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "construct_dist_table: dot,PQ=16,DIM=128",
+            "value": 27424,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compute_distances: 16000,l2,PQ=16,DIM=128",
+            "value": 130168,
+            "range": "± 29",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compute_distances: 16000,cosine,PQ=16,DIM=128",
+            "value": 149942,
+            "range": "± 22",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "compute_distances: 16000,dot,PQ=16,DIM=128",
+            "value": 140443,
+            "range": "± 17",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ScalarQuantizationStorage,chunks=1x10K",
+            "value": 269,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ScalarQuantizationStorage,chunks=32x10K",
+            "value": 290,
+            "range": "± 5",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ScalarQuantizationStorage,chunks=128x10K",
+            "value": 293,
+            "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ScalarQuantizationStorage,chunks=1024x10K",
+            "value": 311,
+            "range": "± 5",
             "unit": "ns/iter"
           }
         ]
